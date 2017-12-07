4e88560b9800bc99300d512ba8cf99ce8902060d
David Cheng <dkcheng10@gmail.com>
2017-12-06 18:26:33 -0800

    Setup and Psudo Code

---
+++ javascript.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+//When Opening the webpage I should be able to
+// Run an ajax call
+// return some search results from ajax
+// make those search results into gifs
+
+
+// After everything is loaded. I should be able to add a new button
+// I need to erase the previous buttons and populate the new buttons so that they do not double up
+
+
+//The gifs loaded should be static until I click on them
+
+
+
+
+
+
+
+
+
+
+
+})
